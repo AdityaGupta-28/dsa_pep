f39362df4a9aac17bad041d8ea2bcac00be9c83f
Aditya <adityagupta112040@gmail.com>
2026-07-02 10:14:05 +0530

    Valid Palindrome2

---
+++ string/palindrome2.java
@@ -0,0 +1,28 @@
+package string;
+import java.util.*;
+public class palindrome2 {
+     public boolean isPalindrome(String s, int i, int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j))return false;
+            i++; j--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+       int i=0; int j= s.length()-1;
+       while(i<j){
+        if(s.charAt(i)!=s.charAt(j)){
+            return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+        }
+        i++; j--;
+       }
+           
+      return true;
+    
+}
+
+public void main(String[] args){
+    String s = "abca";
+    System.out.print(validPalindrome(s));
+}
+}
